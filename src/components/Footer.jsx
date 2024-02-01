8b47f9a0c364463a90eda970ef6e327af13693ab
menchylin <menchymarmol@gmail.com>
2024-02-01 16:55:35 +0800

    test

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,11 @@
 import "./Footer.css";
-import { TbNoCopyright } from "react-icons/Tb";
+// import { TbNoCopyright } from "react-icons/Tb";
 
 const Footer = () => {
   return (
     <footer className="footer-container">
       <div className="footer-sub-container">
-        <TbNoCopyright color="#373636" fontSize="18px" fontWeight="900" />
+        {/* <TbNoCopyright color="#373636" fontSize="18px" fontWeight="900" /> */}
         <span>
           Made by <span className="footer-text-violet">mnchyln</span>. 2023.
         </span>
